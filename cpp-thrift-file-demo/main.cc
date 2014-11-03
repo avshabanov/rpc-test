59699c4fbbf41265b57ae21a7c4cce2ce4d1583f
Alexander Shabanov <avshabanov@gmail.com>
2014-11-02 20:43:54 -0800

    Support for compact protocol

--- cpp-thrift-file-demo/main.cc
+++ cpp-thrift-file-demo/main.cc
@@ -9,8 +9,7 @@
 #include <fcntl.h>
 
 #include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/protocol/TDenseProtocol.h>
-#include <thrift/protocol/TJSONProtocol.h>
+#include <thrift/protocol/TCompactProtocol.h>
 #include <thrift/transport/TTransportUtils.h>
 #include <thrift/transport/TFDTransport.h>
 
@@ -33,27 +32,37 @@ int errm(const std::string& message) {
 }
 
 int usage(char * argv[]) {
-  cerr << "Usage: " << argv[0] << " w|r" << endl;
+  cerr << "Usage: " << argv[0] << " b|c(Binary or Compact) w|r(Write or Read)" << endl;
   return -1;
 }
 
+shared_ptr<TProtocol> createProtocol(const shared_ptr<TTransport>& transport, bool useCompact) {
+  if (useCompact) {
+    return shared_ptr<TProtocol>(new TCompactProtocol(transport));
+  }
+  return shared_ptr<TProtocol>(new TBinaryProtocol(transport));
+}
+
 int main(int argc, char * argv[]) {
-  if (argc != 3) {
+  if (argc != 4) {
     return usage(argv);
   }
 
-  //metrics::Header header;
+  const char * protocolType = argv[1];
+  const char * mode = argv[2];
+  const char * fileName = argv[3];
 
-  if (strcmp(argv[1], "r") == 0) {
-    int fd = open(argv[2], O_RDONLY);
+  bool useCompact = (strcmp(protocolType, "c") == 0);
+
+  if (strcmp(mode, "r") == 0) {
+    int fd = open(fileName, O_RDONLY);
     if (fd < 0) {
       return errm("Failed to open a file");
     }
 
     shared_ptr<TFDTransport> innerTransport(new TFDTransport(fd));
     shared_ptr<TBufferedTransport> transport(new TBufferedTransport(innerTransport));
-
-    shared_ptr<TBinaryProtocol> protocol(new TBinaryProtocol(transport));
+    shared_ptr<TProtocol> protocol = createProtocol(transport, useCompact);
 
     transport->open();
 
@@ -63,15 +72,14 @@ int main(int argc, char * argv[]) {
     transport->close();
 
     cout << "OK: read from file succeeded, header.contentLength=" << header.contentLength << ", header.contentType=" << header.contentType  << endl;
-  } else if (strcmp(argv[1], "w") == 0) {
-    int fd = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR | S_IXUSR);
+  } else if (strcmp(mode, "w") == 0) {
+    int fd = open(fileName, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR | S_IXUSR);
     if (fd < 0) {
       return errm("Failed to open a file");
     }
     shared_ptr<TFDTransport> innerTransport(new TFDTransport(fd));
     shared_ptr<TBufferedTransport> transport(new TBufferedTransport(innerTransport));
-
-    shared_ptr<TBinaryProtocol> protocol(new TBinaryProtocol(transport));
+    shared_ptr<TProtocol> protocol = createProtocol(transport, useCompact);
 
     metrics::Header header;
     header.__set_contentLength(123);
